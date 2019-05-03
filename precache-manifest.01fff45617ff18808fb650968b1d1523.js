18f8d45d99c4d3b794f99efcb20c2d2806325ebd
Candice Thomas <torenesk@gmail.com>
2019-05-03 16:54:05 -0700

    Updates

---
+++ precache-manifest.01fff45617ff18808fb650968b1d1523.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff9023b545f1e3a20a78ac21116e25a",
+    "url": "/pluralsite-authorquiz/index.html"
+  },
+  {
+    "revision": "26336cac674606a5208d",
+    "url": "/pluralsite-authorquiz/static/css/main.acb72bac.chunk.css"
+  },
+  {
+    "revision": "8489c8d50e35251b7360",
+    "url": "/pluralsite-authorquiz/static/js/2.2fc27334.chunk.js"
+  },
+  {
+    "revision": "26336cac674606a5208d",
+    "url": "/pluralsite-authorquiz/static/js/main.55b94a37.chunk.js"
+  },
+  {
+    "revision": "4bf60edcf2291f121d49",
+    "url": "/pluralsite-authorquiz/static/js/runtime~main.ea07f541.js"
+  }
+]);
